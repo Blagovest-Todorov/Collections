2ffda81fbe6cd4c53fc28eb38d8f215c40885329
Blagovest-Todorov <blagovest.stod@abv.bg>
2022-05-11 08:24:29 +0300

    Add project files.

---
+++ LearningTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+namespace LearningTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a collection of ints
+            Collection<int> myColl = new Collection<int>();
+            string a = "[";
+            string b = "]";
+            Console.WriteLine();
+            Console.WriteLine(a + b);
+
+            //var names = new Collection<string>("Peter", "Maria");
+
+
+        }
+    }
+}
